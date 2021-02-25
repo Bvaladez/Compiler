355171524f95338551195b3ce2a52d36f80ca66f
Bradly valadez <d00297297@dmail.dixie.edu>
2021-02-25 13:29:19 -0600

    adedd

---
+++ Symbol.cpp
@@ -0,0 +1,26 @@
+#include "Symbol.h"	
+
+bool SymbolTableClass::Exists(const std::string& s) {
+
+}
+
+void SymbolTableClass::AddEntry(const std::string& s) {
+
+}
+
+int SymbolTableClass::GetValue(const std::string& s) {
+
+}
+
+void SymbolTableClass::SetValue(const std::string& s, int v) {
+
+}
+
+int SymbolTableClass::GetIndex(const std::string& s) {
+
+}
+
+int SymbolTableClass::GetCount() {
+
+}
+
--- Symbol.h
+++ Symbol.h
@@ -1,2 +1,17 @@
 #pragma once
+#include <string>
 
+class SymboolTableClass {
+
+public:
+	bool Exists(const std::string& s);
+	void AddEntry(const std::string& s);
+	int GetValue(const std::string& s);
+	void SetValue(const std::string& s, int v);
+	int GetIndex(const std::string& s);
+	int GetCount();
+
+private:
+
+
+};
